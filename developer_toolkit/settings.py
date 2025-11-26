4b9918975d3567344be5181465daca07e567140d
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-26 11:01:32 +0000

    Install django-taggit and configure settings.py accordingly

--- developer_toolkit/settings.py
+++ developer_toolkit/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
     'resources',
 ]
 
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+TAGGIT_CASE_INSENSITIVE = True
 
 ROOT_URLCONF = 'developer_toolkit.urls'
 
