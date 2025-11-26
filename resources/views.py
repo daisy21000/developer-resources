032e4e2ce5008991f99b4cb03afb133da5e04155
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-26 11:54:11 +0000

    Implement resource listing in index view with a simple HTML template

--- resources/views.py
+++ resources/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Resource, Category
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    resources = Resource.objects.filter(approved=True).order_by('-created_at')
+    categories = Category.objects.filter(published=True).order_by('name')
+    context = {
+        'resources': resources,
+        'categories': categories,
+    }
+    return render(request, 'resources/index.html', context)
