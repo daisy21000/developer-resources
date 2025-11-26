9a8d943a8802906fc49c91b99901df6c30288613
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-26 10:18:17 +0000

    Add resource URLs file and basic index view for the resources app

--- developer_toolkit/urls.py
+++ developer_toolkit/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from resources import urls as resource_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(resource_urls), name='resources_urls'),
 ]
