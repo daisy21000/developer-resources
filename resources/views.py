f3813ea0c7377063e648047965d935c684338081
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-28 12:10:53 +0000

    Add Resource submission functionality and add crispy forms to requirements

--- resources/views.py
+++ resources/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Resource, Category
+from .forms import ResourceForm, CategoryForm
 
 
 # Create your views here.
@@ -20,3 +21,22 @@ def category_detail(request, category_id):
         'resources': resources,
     }
     return render(request, 'resources/category_detail.html', context)
+
+
+def submit_resource(request):
+    if request.method == 'POST':
+        form = ResourceForm(request.POST)
+        if form.is_valid():
+            resource = form.save(commit=False)
+            resource.uploader = request.user
+            resource.approved = False
+            resource.save()
+            form.save_m2m()  # Save tags
+            return HttpResponse("Resource submitted successfully and is pending approval.")
+    else:
+        form = ResourceForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'resources/add_resource.html', context)
