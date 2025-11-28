3cf553ac26538ec5479c05c65a9a1f277d4a5360
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-28 12:24:07 +0000

    Add resource submission page and update navigation links

--- resources/urls.py
+++ resources/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('add/', views.submit_resource, name='add_resource'),
 ]
