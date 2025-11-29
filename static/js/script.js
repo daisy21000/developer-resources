12d7fb37cf30e6bc9419c32fa558a419922dd50d
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-29 11:25:07 +0000

    Refactor login form structure and enhance input field styling

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,11 @@ if (signupForm) {
         field.classList.add('form-control', 'my-2');
     });
 }
+
+const loginForm = document.getElementById('login_form');
+if (loginForm) {
+    const inputFields = loginForm.querySelectorAll('input[type="text"], input[type="password"]');
+    inputFields.forEach(field => {
+        field.classList.add('form-control', 'my-2');
+    });
+}
