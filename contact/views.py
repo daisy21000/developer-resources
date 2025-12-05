448832ce841f67afc6acd1ac70ed0706a6101899
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-12-05 11:15:33 +0000

    Add contact HTML template and render it in view

--- contact/views.py
+++ contact/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def contact(request):
-    return HttpResponse("Contact Page Placeholder")
+    return render(request, 'contact/contact.html')
