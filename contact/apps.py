fa4a4dee6a93849a5517018beafcc2e48f016c4e
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-12-08 11:44:57 +0000

    Add docstrings to ContactConfig and ResourcesConfig for improved clarity

--- contact/apps.py
+++ contact/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ContactConfig(AppConfig):
+    """
+    Provides primary key type for the contact application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
--- resources/apps.py
+++ resources/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ResourcesConfig(AppConfig):
+    """
+    Provides primary key type for the resources application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'resources'
