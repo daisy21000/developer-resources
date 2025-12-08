2ab2358020b4fe8cdbbe39268e4a9963e416880c
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-12-08 11:15:41 +0000

    Add docstring and comment to sort_resources function for improved clarity

--- resources/utils.py
+++ resources/utils.py
@@ -2,6 +2,12 @@ from django.db.models import Count
 
 
 def sort_resources(request, resources):
+    """
+    Sort resources based on user-selected criteria.
+
+    :param request: The HTTP request object containing GET parameters.
+    :param resources: A queryset of :model:`Resource` objects to be sorted.
+    """
     sort_by = request.GET.get('sort_by', 'alphabetical')
     if sort_by == 'alphabetical':
         resources = resources.order_by('name')
@@ -10,5 +16,6 @@ def sort_resources(request, resources):
     elif sort_by == 'oldest':
         resources = resources.order_by('created_at')
     elif sort_by == 'most_favorited':
+        # Annotate each resource with the count of favorites and order by that count
         resources = resources.annotate(num_favorites=Count('favorites', distinct=True)).order_by('-num_favorites')
     return resources
